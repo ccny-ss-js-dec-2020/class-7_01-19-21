63c875e230db1b62bd33cec0093ae3c6d94341f2
Jared Thomas <jcjt929@web-development-teaching.com>
2021-01-19 21:01:53 -0500

    adding in-class exercise solution

--- constraints/exercise/solved/schema.sql
+++ constraints/exercise/solved/schema.sql
@@ -14,3 +14,26 @@
   * Do some inserts that are rejected as well
 
 */
+
+CREATE TABLE performers (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  age INT
+);
+
+CREATE TABLE moviesTwo (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  PerformerId INT NOT NULL,
+  FOREIGN KEY (PerformerId) REFERENCES performers(id)
+);
+
+INSERT INTO performers (name, age) VALUES ('Nicolas Cage', 55);
+INSERT INTO performers (name, age) VALUES ('John Malkovich', 60);
+
+INSERT INTO moviesTwo (name, PerformerId) VALUES ('Con Air', 1);
+INSERT INTO moviesTwo (name, PerformerId) VALUES ('Con Air', 2);
+INSERT INTO moviesTwo (name, PerformerId) VALUES ('Leaving Las Vegas', 1);
+INSERT INTO moviesTwo (name, PerformerId) VALUES ('Rounders', 2);
+
+SELECT * FROM moviesTwo WHERE name = 'Con Air';
